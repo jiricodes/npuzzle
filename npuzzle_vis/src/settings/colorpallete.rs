28935506bd000a35405820b67add06892b5c2753
Jiri Novotny <hello@jiricodes.com>
2022-03-08 22:15:56 +0200

    color palletes previewer

--- npuzzle_vis/src/settings/colorpallete.rs
+++ npuzzle_vis/src/settings/colorpallete.rs
@@ -4,6 +4,8 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 struct CollorPalletePreviewTag;
+#[derive(Component)]
+struct CollorPalletePreviewFontTag;
 
 pub struct ColorPallete {
 	background: Color,
@@ -46,6 +48,10 @@ impl ColorPallete {
 	pub fn tile_win(&self) -> Color {
 		self.tile_win
 	}
+
+	pub fn all() -> Vec<Self> {
+		vec![Self::default(), Self::YELLOW]
+	}
 }
 
 pub struct ColorPalleteTestPlugin;
@@ -55,10 +61,15 @@ fn camera_setup(mut commands: Commands) {
 	commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
-fn preview(mut cmds: Commands, asset_server: Res<AssetServer>, colorpallete: Res<ColorPallete>) {
+fn preview(
+	mut cmds: Commands,
+	asset_server: Res<AssetServer>,
+	palletes: Res<Palletes>,
+	pid: Res<CurrentId>,
+) {
+	let colorpallete = palletes.0.iter().nth(pid.0).unwrap();
 	let preview_size: f32 = 200.0;
-	let frame_size = Vec2::new(preview_size, preview_size);
-	let tile_size = Vec2::new(frame_size.x * 0.5, frame_size.y * 0.5);
+	let tile_size = Vec2::new(preview_size, preview_size);
 	let font_handle: Handle<Font> = asset_server.load(FONT_PATH);
 	let text_style = TextStyle {
 		font: font_handle,
@@ -71,43 +82,75 @@ fn preview(mut cmds: Commands, asset_server: Res<AssetServer>, colorpallete: Res
 	};
 	cmds.spawn_bundle(SpriteBundle {
 		sprite: Sprite {
-			color: colorpallete.background(),
-			custom_size: Some(frame_size),
+			color: colorpallete.tile_default(),
+			custom_size: Some(tile_size),
 			..Default::default()
 		},
 		..Default::default()
 	})
-	.with_children(|top| {
-		top.spawn_bundle(SpriteBundle {
-			sprite: Sprite {
-				color: colorpallete.tile_default(),
-				custom_size: Some(tile_size),
-				..Default::default()
-			},
-			transform: Transform {
-				translation: Vec3::new(0.0, 0.0, 1.0),
-				..Default::default()
-			},
-			..Default::default()
-		})
-		.with_children(|parent| {
-			parent.spawn_bundle(Text2dBundle {
+	.with_children(|parent| {
+		parent
+			.spawn_bundle(Text2dBundle {
 				text: Text::with_section("1", text_style.clone(), text_alignment),
 				transform: Transform {
-					translation: Vec3::new(0.0, 0.0, 2.0),
+					translation: Vec3::new(0.0, 0.0, 1.0),
 					..Default::default()
 				},
 				..Default::default()
-			});
-		});
+			})
+			.insert(CollorPalletePreviewFontTag);
 	})
 	.insert(CollorPalletePreviewTag);
 }
 
+fn update_colors(
+	wp: Res<WinPreview>,
+	palletes: Res<Palletes>,
+	pid: Res<CurrentId>,
+	mut bg: ResMut<ClearColor>,
+	mut q: Query<&mut Sprite, With<CollorPalletePreviewTag>>,
+) {
+	let pallete = palletes.0.iter().nth(pid.0).unwrap();
+	bg.0 = pallete.background();
+	let color = if wp.0 {
+		pallete.tile_win
+	} else {
+		pallete.tile_default
+	};
+	for mut sprite in q.iter_mut() {
+		sprite.color = color;
+	}
+}
+
+fn keyboard_input(
+	keys: Res<Input<KeyCode>>,
+	mut wp: ResMut<WinPreview>,
+	mut pid: ResMut<CurrentId>,
+	palletes: Res<Palletes>,
+) {
+	if keys.just_released(KeyCode::Space) {
+		wp.0 = !wp.0
+	}
+	if keys.just_released(KeyCode::Left) {
+		pid.0 = (palletes.0.len() - 1).min(pid.0.overflowing_sub(1).0);
+	}
+	if keys.just_released(KeyCode::Right) {
+		pid.0 = (palletes.0.len() - 1).min(pid.0 + 1);
+	}
+}
+
+struct WinPreview(pub bool);
+struct Palletes(Vec<ColorPallete>);
+struct CurrentId(usize);
+
 impl Plugin for ColorPalleteTestPlugin {
 	fn build(&self, app: &mut App) {
-		app.insert_resource(ColorPallete::YELLOW)
+		app.insert_resource(Palletes(ColorPallete::all()))
+			.insert_resource(CurrentId(0))
+			.insert_resource(WinPreview(false))
 			.add_startup_system(camera_setup)
-			.add_startup_system(preview);
+			.add_startup_system(preview)
+			.add_system(update_colors)
+			.add_system(keyboard_input);
 	}
 }
